8195e8f153d028e166c0c726cef0a83df7a67530
Ida Hägerth <idiis@live.com>
2024-02-20 15:03:57 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿class NoDuplicates {
+    static void Main(string[] args) {
+        Console.WriteLine("Enter line: ");
+        string input = Console.ReadLine() ?? string.Empty;
+
+        if (input.Length > 80) {
+            Console.WriteLine("Input exceeds limit");
+            return;
+        }
+
+        if (!string.IsNullOrEmpty(input) && input.Any(char.IsUpper)) {
+            var words = input.Split(' ');
+            var wordSet = new HashSet<string>();
+            bool duplicate = false;
+
+            foreach (var word in words) {
+                if (!wordSet.Add(word)) {
+                    duplicate = true;
+                    break;
+                }
+            }
+            Console.WriteLine(duplicate ? "NO" : "YES");
+        }
+        else {
+            Console.WriteLine("Invalid input");
+        }
+    }
+}
